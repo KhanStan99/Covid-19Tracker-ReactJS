d32d30b8ca21cda0e244e487ed291bbe82fd3cdd
KhanStan99 <soubankhan3@gmail.com>
2020-04-11 02:59:58 +0530

    Changed UI - Added cities as well

--- src/App.js
+++ src/App.js
@@ -4,41 +4,81 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import axios from "axios";
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from "@material-ui/core/Paper";
+import "./App.css";
+import Avatar from "@material-ui/core/Avatar";
+import Chip from "@material-ui/core/Chip";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import "./App.css";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+  green: {
+    color: "#6eff9e",
+    backgroundColor: "#6eff9e",
+  },
+  red: {
+    color: "#ff4567",
+    backgroundColor: "#ff4567",
+  },
+  blue: {
+    color: "#91b6ff",
+    backgroundColor: "#91b6ff",
   },
   title: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   rootCard: {
-    width: "100%"
+    width: "100%",
   },
   inline: {
-    display: "inline"
-  }
+    display: "inline",
+  },
+  chips: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(0.5),
+    },
+  },
 }));
 
 function App() {
   const classes = useStyles();
   const [globalData, setGlobalData] = useState(null);
+  const [cityData, setCityData] = useState(null);
 
   if (!globalData) {
     axios
       .get("https://api.covid19india.org/data.json")
-      .then(function(response) {
+      .then(function (response) {
         setGlobalData(response.data.statewise);
       })
-      .catch(function(error) {
+      .catch(function (error) {
+        // reject(error);
+      });
+  }
+
+  if (!cityData) {
+    axios
+      .get("https://api.covid19india.org/v2/state_district_wise.json")
+      .then(function (response) {
+        setCityData(response.data);
+      })
+      .catch(function (error) {
         // reject(error);
       });
   }
@@ -57,7 +97,12 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
-      {globalData ? <TotalCases globalData={globalData} /> : <p>Loading....</p>}
+
+      {globalData ? (
+        <TotalCases globalData={globalData} cityData={cityData} />
+      ) : (
+        <p>Loading....</p>
+      )}
 
       <footer style={{ textAlign: "center" }}>
         <a href="https://www.trentweet.in">Posted by Trentweet</a>
@@ -73,54 +118,83 @@ function App() {
 }
 
 function TotalCases(props) {
+  const classes = useStyles();
   return (
     <TableContainer component={Paper}>
-      <Table aria-label="simple table">
+      {props.globalData.map((testItem) => (
+        <ExpansionPanel key={testItem.state}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <div>
+              <b>{testItem.state}</b>
+              <br />
+              <div className={classes.chips}>
+                <Chip
+                  label={formatNumber(testItem.confirmed)}
+                  avatar={<Avatar>T</Avatar>}
+                />
+                <Chip
+                  label={formatNumber(testItem.active)}
+                  avatar={<Avatar className={classes.blue}>A</Avatar>}
+                />
+
+                <Chip
+                  label={formatNumber(testItem.recovered)}
+                  avatar={<Avatar className={classes.green}>R</Avatar>}
+                />
+
+                <Chip
+                  avatar={<Avatar className={classes.red}>D</Avatar>}
+                  label={formatNumber(testItem.deaths)}
+                />
+              </div>
+              <small>last update: {testItem.lastupdatedtime}</small>
+            </div>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            {testItem.state !== "Total" && props.cityData ? (
+              <DetailedPanel state={testItem.state} cityList={props.cityData} />
+            ) : (
+              "Nothing to show"
+            )}
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      ))}
+    </TableContainer>
+  );
+}
+
+function DetailedPanel(props) {
+  let districts = [];
+  props.cityList.forEach((element) => {
+    if (props.state === element.state) {
+      districts = element.districtData;
+    }
+  });
+  districts.sort(function (a, b) {
+    return b.confirmed - a.confirmed;
+  });
+
+  return (
+    <TableContainer component={Paper}>
+      <Table size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
-            <TableCell>State</TableCell>
-            <TableCell>Confirmed</TableCell>
-            <TableCell>Active</TableCell>
-            <TableCell>Recovered</TableCell>
-            <TableCell>Deaths</TableCell>
-            <TableCell>Date</TableCell>
+            <TableCell>District</TableCell>
+            <TableCell align="right">Confirmed</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {props.globalData.map(testItem => (
-            <TableRow key={testItem.state}>
-              <TableCell component="th" scope="testItem.confirmed">
-                {testItem.state}
+          {districts.map((item) => (
+            <TableRow key={item.district}>
+              <TableCell component="th" scope="row">
+                {item.district}
               </TableCell>
-              <TableCell component="th" scope="testItem.confirmed">
-                <span className="cases">
-                  {formatNumber(testItem.confirmed)} [+
-                  {formatNumber(testItem.deltaconfirmed)}]
-                </span>
-              </TableCell>
-              <TableCell component="th" scope="testItem.active">
-                <span className="active">
-                  {formatNumber(testItem.active)}
-                </span>
-              </TableCell>
-              <TableCell
-                className="recovered"
-                component="th"
-                scope="testItem.recovered"
-              >
-                <span className="recovered">
-                  {formatNumber(testItem.recovered)} [+
-                  {formatNumber(testItem.deltarecovered)}]
-                </span>
-              </TableCell>
-              <TableCell component="th" scope="testItem.deaths">
-                <span className="deaths">
-                  {formatNumber(testItem.deaths)} [+
-                  {formatNumber(testItem.deltadeaths)}]
-                </span>
-              </TableCell>
-              <TableCell component="th" scope="testItem.lastupdatedtime">
-                {testItem.lastupdatedtime}
+              <TableCell align="right">
+                {formatNumber(item.confirmed)}
               </TableCell>
             </TableRow>
           ))}
